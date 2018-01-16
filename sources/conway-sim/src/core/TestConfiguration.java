1b364b21d768a7acd5771f4b3165f0786a2d94dd
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2018-01-16 01:22:08 +0100

    Eclipse Project Created

---
+++ sources/conway-sim/src/core/TestConfiguration.java
@@ -0,0 +1,16 @@
+package core;
+
+/**
+ * A test.
+ *
+ */
+public class TestConfiguration {
+
+    /** Main.
+     * @param args passed
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
