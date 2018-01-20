6cae7024f010a21044bc367c43a9af1a2ecaeb19
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2018-01-20 11:45:38 +0100

    Added dummy test classes and packages

---
+++ sources/conway-sim/src/core/model/TestModel.java
@@ -0,0 +1,10 @@
+package core.model;
+
+public abstract class TestModel {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ sources/conway-sim/src/view/swing/TestView.java
@@ -0,0 +1,10 @@
+package view.swing;
+
+public class TestView {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
