5ad1664b620034dd7634b1413ed75e91a4469768
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2018-02-02 11:27:01 +0100

    Added CellEnviroment methods

--- sources/conway-sim/src/core/model/CellEnvironment.java
+++ sources/conway-sim/src/core/model/CellEnvironment.java
@@ -1,10 +1,22 @@
 package core.model;
 
 /**
- * 
- * 
+ * This class uses two strategies passed to constructor to determine if the cell should die or not based on the number of neighbors.
  *
  */
 public interface CellEnvironment {
 
+    /**
+     * Check for cell birth.
+     * @param neighbors of this cell
+     * @return true if the cell should be born
+     */
+    boolean checkCellBorn(int neighbors);
+
+    /**
+     * Check for cell death.
+     * @param neighbors of this cell
+     * @return true if the cell should die
+     */
+    boolean checkCellDeath(int neighbors);
 }
