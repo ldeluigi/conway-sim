428ad8ab8a0f6a6dc01560f8e675ba9f09bba712
Mirko Fabbri <mirko.fabbri3@studio.unibo.it>
2018-02-02 10:43:19 +0100

    add other basic interfaces
    add interfaces about various environments ant the Global one

---
+++ sources/conway-sim/src/core/model/Environment.java
@@ -0,0 +1,10 @@
+package core.model;
+
+/**
+ * 
+ * 
+ *
+ */
+public interface Environment {
+
+}
---
+++ sources/conway-sim/src/core/model/GlobalEnvironment.java
@@ -0,0 +1,31 @@
+package core.model;
+
+/**
+ * This is the main environment for the generation.
+ * 
+ *
+ */
+
+public interface GlobalEnvironment {
+
+    /**
+     * 
+     * @return the current height of the environment
+     */
+    int getHeight();
+
+    /**
+     * 
+     * @return the current height
+     */
+    int getWidth();
+
+
+    /**
+     * 
+     * @param x height of the environment to be used
+     * @param y width of the environment to be used
+     * @param env the environment to be used
+     */
+    void setEnvironment(int x, int y, Environment env);
+}
