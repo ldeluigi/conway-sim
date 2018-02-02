32ed6040e80665793881bab69a7c81e8793a94b7
Luca Bandini <luca.bandini6@studio.unibo.it>
2018-02-02 10:48:24 +0100

    Added more dummy methods and mementos

--- sources/conway-sim/src/controller/book/Book.java
+++ sources/conway-sim/src/controller/book/Book.java
@@ -1,12 +1,27 @@
 package controller.book;
-
+/**
+ * 
+ * @author LDLM Team
+ *
+ */
 public interface Book {
 /**
  * 
  */
 	void dummyMethod();
-	
-	
+	/**
+	 * 
+	 */
 	void dummyMethod2();
+	
+	/**
+	 * This method will get the list of patterns to be dragged on the main panel
+	 */
+	void getListPatterns();
+	
+	/**
+	 * This method returns the lenght of the patterns library
+	 */	
+	int getListLenght();
 	
 }
