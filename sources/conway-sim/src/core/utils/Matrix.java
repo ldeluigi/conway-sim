db9cd03236d27a69ac5cfd67993734c5b4358a5d
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2018-02-02 10:06:32 +0100

    Added Matrix interface in core.utils

---
+++ sources/conway-sim/src/core/utils/Matrix.java
@@ -0,0 +1,56 @@
+package core.utils;
+
+import java.util.function.Function;
+
+/**
+ * A generic matrix with useful utility methods.
+ * 
+ * @param <X>
+ */
+public interface Matrix<X> {
+
+    /**
+     * Returns the item from the given position.
+     * 
+     * @param row of the item
+     * @param column of the item
+     * @return the item
+     */
+    X get(int row, int column);
+
+    /**
+     * Rotates the matrix by 90 degrees clockwise.
+     */
+    void rotateClockwise();
+
+    /**
+     * Sets the given value at the given position.
+     * 
+     * @param row of the item
+     * @param column of the item
+     * @param value of the item
+     */
+    void set(int row, int column, X value);
+
+    /**
+     * Returns the height of the matrix.
+     * 
+     * @return the height of the matrix
+     */
+    int getHeight();
+
+    /**
+     * Returns the width of the matrix.
+     * 
+     * @return the width of the matrix
+     */
+    int getWidth();
+
+    /**
+     * A method to map this matrix to another one of the same dimension.
+     * @param mapper to create elements of the new matrix.
+     * @param <Y> the type of the new matrix
+     * @return the new mapped matrix
+     */
+    <Y> Matrix<Y> map(Function<? super X, ? extends Y> mapper);
+}
