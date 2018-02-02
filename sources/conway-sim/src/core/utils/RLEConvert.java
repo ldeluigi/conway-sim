e505e804e4c08574d4384dd21eddb69a9fd3adf7
Luca Bandini <luca.bandini6@studio.unibo.it>
2018-02-02 09:52:09 +0100

    Added util RLEConvert

---
+++ sources/conway-sim/src/core/utils/RLEConvert.java
@@ -0,0 +1,8 @@
+package core.utils;
+
+public interface RLEConvert {
+
+	//This will contain methods to convert a given matrix into a RLE Format
+	//http://www.conwaylife.com/w/index.php?title=Run_Length_Encoded
+	//NB: This will be used also as I/O method and SaveToFile
+}
