d5e33b9f259658e78e97d19d610e3aebc5beeefc
Mirko Fabbri <mirko.fabbri3@studio.unibo.it>
2018-02-17 11:27:32 +0100

    GenerationImpl
    addes an implementation for the interface Generation

--- sources/conway-sim/src/core/model/CellImpl.java
+++ sources/conway-sim/src/core/model/CellImpl.java
@@ -11,6 +11,13 @@ public class CellImpl implements Cell {
     private Status current;
 
     /**
+     * Constructor for a new Cell.
+     * @param newStatus is the status to be assumed the first time
+     */
+    public CellImpl(final Status newStatus) {
+        this.current = newStatus;
+    }
+    /**
      * setStatus is the method to invoke in order to change the current status of the cell.
      * @param nextstatus is the new status to be assumed. 
      */
