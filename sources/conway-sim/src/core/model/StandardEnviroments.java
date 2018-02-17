ca51f804b4311d0ec505469fbfa55884292ba6b6
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2018-02-17 10:57:42 +0100

    Refactoring of mispelled interfaces + StandardEnviroments

---
+++ sources/conway-sim/src/core/model/StandardEnviroments.java
@@ -0,0 +1,31 @@
+package core.model;
+
+import java.util.function.Function;
+
+/**
+ * Enumeration for common rule sets used in Conway's Game of Life.
+ * 
+ */
+public enum StandardEnviroments implements CellEnviroment {
+    /**
+     * This is the classic rule set of Conway's Game of Life.
+     */
+    STANDARD(n -> n == 3, n -> n < 2 || n > 3);
+
+    private final Function<Integer, Boolean> checkBorn;
+    private final Function<Integer, Boolean> checkDeath;
+
+    StandardEnviroments(final Function<Integer, Boolean> checkBorn, final Function<Integer, Boolean> checkDeath) {
+        this.checkBorn = checkBorn;
+        this.checkDeath = checkDeath;
+    }
+    @Override
+    public boolean checkCellBorn(final int neighbors) {
+        return this.checkBorn.apply(neighbors);
+    }
+
+    @Override
+    public boolean checkCellDeath(final int neighbors) {
+        return this.checkDeath.apply(neighbors);
+    }
+}
