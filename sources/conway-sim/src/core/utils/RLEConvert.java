2babbd3d1a633d13bcaad60ed6c7c2d85626ac41
Luca Bandini <luca.bandini6@studio.unibo.it>
2018-02-22 23:34:59 +0100

    Test commit
    Test commit for Telegram Bot

--- sources/conway-sim/src/core/utils/RLEConvert.java
+++ sources/conway-sim/src/core/utils/RLEConvert.java
@@ -20,6 +20,7 @@ public class RLEConvert {
     //This will contain methods to convert a given matrix into a RLE Format
     //http://www.conwaylife.com/w/index.php?title=Run_Length_Encoded
     //NB: This will be used also as I/O method and SaveToFile
+    //H
     /**
      * 
      * @param fileName name of the file to be loaded
