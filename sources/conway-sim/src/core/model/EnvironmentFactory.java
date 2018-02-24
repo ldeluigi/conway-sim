806024e930b33de4961efb1d71069797c4554128
Mirko <mirko.fabbri3@studio.unibo.it>
2018-02-24 12:43:42 +0100

    added EnvironmentFactory
    switched Environment to its factory implementation

--- sources/conway-sim/src/core/model/EnvironmentFactory.java
+++ sources/conway-sim/src/core/model/EnvironmentFactory.java
@@ -1,5 +1,6 @@
 package core.model;
 
+import core.utils.ListMatrix;
 import core.utils.Matrix;
 
 /**
@@ -15,18 +16,54 @@ public final class EnvironmentFactory {
      * @return the Environment as specified
      */
     public static Environment standardRules(final int width, final int height) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+        return new Environment() {
 
+            private int envHeight = height; 
+            private int envWidth = width;
+
+			@Override
+			public int getHeight() {
+				return this.envHeight;
+			}
+
+			@Override
+			public int getWidth() {
+				return this.envWidth;
+			}
+
+			@Override
+			public CellEnvironment getCellEnvironment(final int x, final int y) {
+				return StandardCellEnvironments.STANDARD;
+			}
+
+        };
+    }
     /**
      * A method that creates an {@link Environment} from the given {@link CellEnvironment} matrix.
      * @param envMatrix a {@link Matrix<CellEnvironment>}
      * @return the Environment based on the given matrix
      */
     public static Environment from(final Matrix<CellEnvironment> envMatrix) {
-        // TODO Auto-generated method stub
-        return null;
+        return new Environment() {
+
+        	private Matrix<CellEnvironment> matrix = envMatrix;
+
+        	@Override
+			public int getHeight() {
+				return this.matrix.getHeight();
+			}
+
+			@Override
+			public int getWidth() {
+				return this.matrix.getWidth();
+			}
+
+			@Override
+			public CellEnvironment getCellEnvironment(final int x, final int y) {
+				return this.matrix.get(x, y);
+			}
+
+        };
     }
 
     /**
@@ -37,8 +74,31 @@ public final class EnvironmentFactory {
      * @return the Environment as specified
      */
     public static Environment fill(final int width, final int height, final CellEnvironment cellEnv) {
-        // TODO Auto-generated method stub
-        return null;
+        return new Environment() {
+
+        	private int envHeight = height;
+        	private int envWidth = width;
+        	private CellEnvironment env = cellEnv;
+        	private Matrix<CellEnvironment> matrix = new ListMatrix<CellEnvironment>(this.envWidth, this.envHeight, () -> this.env);
+
+        	@Override
+			public int getHeight() {
+				// TODO Auto-generated method stub
+				return this.envHeight;
+			}
+
+			@Override
+			public int getWidth() {
+				// TODO Auto-generated method stub
+				return this.envWidth;
+			}
+
+			@Override
+			public CellEnvironment getCellEnvironment(final int x, final int y) {
+				return this.matrix.get(x, y);
+			}
+
+        };
     }
 
 }
