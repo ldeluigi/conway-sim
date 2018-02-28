c6b9270ef931ef977af7d7750a556e07d27a5147
Evangelisti Davide <davide.evangelisti2@studio.unibo.it>
2018-03-01 00:16:45 +0100

    sandbox modify

--- sources/conway-sim/src/view/swing/sandbox/Sandbox.java
+++ sources/conway-sim/src/view/swing/sandbox/Sandbox.java
@@ -1,12 +1,14 @@
 package view.swing.sandbox;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import view.swing.GUI;
+import view.swing.DesktopGUI;
+import view.swing.book.BookFrame;
 
 /**
  * 
@@ -21,19 +23,29 @@ public class Sandbox extends JPanel {
     private final JLabel label = new JLabel("this is Sandbox area");
     private final GenerationPanel generationPanel = new GenerationPanel();
     private final JButton bBook = new JButton("book");
+    private final BookFrame book = new BookFrame();
 
     /**
      * 
      * @param maingui the mainGui that call this SandBox
      */
-    public Sandbox(final GUI maingui) {
+    public Sandbox(final DesktopGUI maingui) {
+
+        final JPanel eastButtonPanel = new JPanel(new FlowLayout());
         this.setLayout(new BorderLayout());
         this.add(label, BorderLayout.CENTER);
-        this.add(bBook, BorderLayout.EAST);
         this.generationPanel.setLayoutSize(this.getSize(), 10);
-        this.add(generationPanel, BorderLayout.NORTH);
+        eastButtonPanel.add(generationPanel);
+        eastButtonPanel.add(bBook);
+        this.add(eastButtonPanel, BorderLayout.EAST);
+        bBook.addActionListener(e -> book());
+
         final JButton bExit = new JButton("EXIT");
-        bExit.addActionListener(e -> maingui.backToMainMenu());
         this.add(bExit, BorderLayout.SOUTH);
+        bExit.addActionListener(e -> maingui.backToMainMenu());
     }
+
+	private void book() {
+		
+	}
 }
