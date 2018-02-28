10c33e1253af13ef8c769dddde5961de253e692d
Evangelisti Davide <davide.evangelisti2@studio.unibo.it>
2018-02-28 17:09:04 +0100

    GenerationControllerImpl first implementation added

---
+++ sources/conway-sim/src/controller/generation/GenerationControllerImpl.java
@@ -0,0 +1,36 @@
+package controller.generation;
+
+public class GenerationControllerImpl implements GenerationController {
+
+    @Override
+    public void startGameWithGeneration() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void end() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setSleepTime(Long timeSleep) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void loadOldGeneration(Long generationNumber) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+}
