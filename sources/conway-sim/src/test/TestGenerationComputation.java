d71ca882399cd1c28c72c0e2107bbb7304e1b74a
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2018-03-02 22:24:42 +0100

    Added Generation multithreading computation

--- sources/conway-sim/src/test/TestGenerationComputation.java
+++ sources/conway-sim/src/test/TestGenerationComputation.java
@@ -52,4 +52,21 @@ public class TestGenerationComputation {
             assertEquals(AFTER_THREE_STANDARD, Generations.compute(start, 3).getAliveMatrix());
             assertEquals(Generations.compute(start, 20).getAliveMatrix(), Generations.compute(start, 13).getAliveMatrix());
      }
+    
+    @Test
+    void testOneComputationMultithread() {
+        Environment env = EnvironmentFactory.standardRules(BEFORE.getWidth(), BEFORE.getHeight());
+        Generation start = GenerationFactory.from(BEFORE.map(b -> new CellImpl(b ? ALIVE : DEAD)), env);
+        assertEquals(AFTER_STANDARD, Generations.compute(start, true).getAliveMatrix());
+        System.out.println("First:\n" + start.getAliveMatrix().map(b -> b ? "■" : "□") + "\nSecond:\n" + Generations.compute(start, true).getAliveMatrix().map(b -> b ? "■" : "□"));
+    }
+
+    @Test
+    void testSomeComputationsMultithread() {
+            Environment env = EnvironmentFactory.standardRules(BEFORE.getWidth(), BEFORE.getHeight());
+            Generation start = GenerationFactory.from(BEFORE.map(b -> new CellImpl(b ? ALIVE : DEAD)), env);
+            assertEquals(AFTER_THREE_STANDARD, Generations.compute(start, true, 3).getAliveMatrix());
+            assertEquals(Generations.compute(start, 20).getAliveMatrix(), Generations.compute(start, true, 13).getAliveMatrix());
+     }
+
 }
