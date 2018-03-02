f3b4fe1a9ea12da5e2c30ee6ec1ad3686d76e767
Mirko <mirko.fabbri3@studio.unibo.it>
2018-03-02 12:47:18 +0100

    improved GridPanel
    added a method to color the grid and some minor improvements

--- sources/conway-sim/src/view/swing/sandbox/GridPanel.java
+++ sources/conway-sim/src/view/swing/sandbox/GridPanel.java
@@ -1,6 +1,7 @@
 package view.swing.sandbox;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -9,11 +10,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
+import core.utils.ListMatrix;
 import core.utils.Matrix;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
 
 /**
  * 
@@ -22,58 +21,59 @@ import java.util.stream.IntStream;
 public class GridPanel extends JScrollPane {
 
     private static final long serialVersionUID = -1;
-    private static final int DIM = 7;
+    private static final int DIMENSION = 5;
 
     private final JPanel grid;
-    private final List<List<JLabel>> labels = new ArrayList<>();
-    final GridBagConstraints c;
 
     /**
      * 
      */
     public GridPanel() {
-        super();
-         this.c = new GridBagConstraints();
+      /*final Matrix<JLabel> labels = new ListMatrix<>(100, 100, () -> {
+            JLabel l = new JLabel(" L ");
+            l.setPreferredSize(new Dimension(GridPanel.DIMENSION, GridPanel.DIMENSION));
+            l.setForeground(Color.BLACK);
+            return l;
+        }); */
+
         this.grid = new JPanel(new GridBagLayout());
-        this.c.fill = GridBagConstraints.NONE;
-        this.c.weightx = 0.5;
-        this.c.weighty = 0.5;
-        final List<JLabel> temp = new ArrayList<>();
+        final GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 0.5;
+        c.weighty = 0.5;
         for (int i = 0; i < 100; i++) {
             for (int j = 0; j < 100; j++) {
-               temp.add(new JLabel(" "));
+                /*final JLabel lab = new JLabel("A");
+                lab.setPreferredSize(new Dimension(GridPanel.DIMENSION, GridPanel.DIMENSION));
+                lab.setForeground(new Color(i * 1000 + j));*/
+                c.gridx = i;
+                c.gridy = j;
+                this.grid.add(/*labels.get(i, j)*/ new JLabel(" L") /*lab*/, c);
             }
-            this.labels.add(temp);
         }
-
-        IntStream.range(0, 100).forEach(i -> {
-            IntStream.range(0, 100).forEach(j -> {
-                this.labels.get(i).get(j).setForeground(new Color(i));
-                this.labels.get(i).get(j).setPreferredSize(new Dimension(DIM, DIM));
-                this.c.gridx = i;
-                this.c.gridy = j;
-                this.grid.add(this.labels.get(i).get(j), c);
-            });
-        });
         this.setViewportView(this.grid);
         this.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         this.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        this.setWheelScrollingEnabled(true);
+        //this.colorGrid();
+    }
+
+    private void colorGrid() {
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                final Component comp = this.grid.getComponentAt(i, j);
+                comp.setForeground(new Color((i * 1000) + j));
+                comp.setPreferredSize(new Dimension(GridPanel.DIMENSION, GridPanel.DIMENSION));
+            }
+        }
     }
 
     /**
-     * Abcde fghi jklmnopqrst uvwxyz.
-     * @param painting is the to.
+     * Anfjbòk rebjq   rpàa  agniertyibtucq.
+     * @param paint is the to.
      */
-    public void drawMatrix(final Matrix<Color> painting) {
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-               this.labels.get(i).get(j).setForeground(painting.get(i, j));
-               this.labels.get(i).get(j).setPreferredSize(new Dimension(DIM, DIM));
-               this.c.gridx = i;
-               this.c.gridy = j;
-               this.grid.add(this.labels.get(i).get(j), c);
-            } 
-        }
+    public void paintMatrix(final Matrix<Color> paint) {
+        // cose che non funzionano
     }
 
 }
