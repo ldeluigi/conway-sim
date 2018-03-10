a785af1a65a97bd1f78e31e856d54530387b56c7
Luca Bandini <luca.bandini6@studio.unibo.it>
2018-03-10 18:53:53 +0100

    Fixed Name loading issues
    Now the name of the pattern gets loaded from the RLE file (#N tag) if present, otherwise it uses the filename as before.
    Still to be finalized.

--- sources/conway-sim/src/controller/io/RecipeLoader.java
+++ sources/conway-sim/src/controller/io/RecipeLoader.java
@@ -1,6 +1,9 @@
 package controller.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import controller.book.RecipeBookImpl;
@@ -12,18 +15,36 @@ public class RecipeLoader {
     private RecipeBookImpl recipebook;
     /** This class parses all the files in the preset folder.
      *  than it loads it in the recipebook.
+     * @throws IOException 
      */
     public RecipeLoader() {
         File folder = new File("./src/test/recipebook");
         File[] listOfFiles = folder.listFiles();
         ArrayList<String> filespaths = new ArrayList<String>();
         this.recipebook = new RecipeBookImpl();
-
+        String testLine = "testLine: NOT_INITIALIZED";
+        FileReader namereader;
+        BufferedReader in;
+        Boolean flagName;
         try {
             for (File file : listOfFiles) {
                 if (file.isFile()) {
+                    flagName = false;
                     filespaths.add(file.getAbsolutePath());
-                    this.recipebook.addRecipe(file.toString(), file.getName());
+                    System.out.println(file.getAbsolutePath());
+                    try {
+                        namereader = new FileReader(file);
+                        in = new BufferedReader(namereader);
+                        testLine = in.readLine();
+                        if (testLine.startsWith("#N")) {
+                            flagName = true;
+                            testLine = testLine.split("#N ")[1];
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("testLine: " + testLine);
+                    this.recipebook.addRecipe(file.toString(), flagName ? testLine : file.getName());
                 }
             }
         } catch (NullPointerException e) {
