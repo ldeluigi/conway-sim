c4157d63ab8c41f974e501f4922920f8746d8172
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2018-03-12 23:59:53 +0100

    Fixed test

--- sources/conway-sim/src/test/TestGUIBook.java
+++ sources/conway-sim/src/test/TestGUIBook.java
@@ -1,6 +1,7 @@
 package test;
 
 import view.swing.book.*;
+import view.swing.sandbox.GridPanel;
 import controller.editor.GridEditorImpl;
 import view.swing.MainGUI;
 
@@ -9,7 +10,7 @@ public class TestGUIBook {
 
         public static void main(String[] args) {
             MainGUI m = new MainGUI();
-            BookFrame bf = new BookFrame(new GridEditorImpl(100, 100));
+            BookFrame bf = new BookFrame(new GridEditorImpl(new GridPanel(100, 100, m)));
             m.popUpFrame(bf);
         }
 }
