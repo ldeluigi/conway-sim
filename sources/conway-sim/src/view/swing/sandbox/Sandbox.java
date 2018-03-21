58b9604d2a0d974d12e55ade20808626458c249f
Evangelisti Davide <davide.evangelisti2@studio.unibo.it>
2018-03-21 15:30:17 +0100

    fixed checkstyle in sandbox

--- sources/conway-sim/src/view/swing/sandbox/Sandbox.java
+++ sources/conway-sim/src/view/swing/sandbox/Sandbox.java
@@ -3,7 +3,6 @@ package view.swing.sandbox;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.Toolkit;
 import java.util.Objects;
 import javax.swing.JButton;
 import javax.swing.JLabel;
