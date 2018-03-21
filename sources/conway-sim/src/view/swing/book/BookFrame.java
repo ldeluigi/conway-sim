bb8b11b8fb6c26bf1cfe20eaacea3381b3de49dd
Luca Bandini <luca.bandini6@studio.unibo.it>
2018-03-21 13:03:33 +0100

    Added Pattern Preview Feature
    Working finally :):):):)

--- sources/conway-sim/src/view/swing/book/BookFrame.java
+++ sources/conway-sim/src/view/swing/book/BookFrame.java
@@ -23,6 +23,7 @@ import controller.io.RLEConvert;
 import controller.io.RecipeLoader;
 import core.model.Status;
 import core.utils.ListMatrix;
+import core.utils.Matrices;
 import core.utils.Matrix;
 import view.swing.sandbox.GridPanel;
 /**
@@ -38,9 +39,11 @@ public class BookFrame extends JInternalFrame {
 
 
 
-    private static final int FRAME_WIDTH = 250;
+    private static final int FRAME_WIDTH = 500;
     //private static final int HEIGHT = 280;
-    private static final int HEIGHTOFCELL = 30;
+    private static final int HEIGHTOFCELL = 50;
+    private static final int INITIAL_GRID_SIZE = 50;
+    private static final int GRID_TO_CELL_RATIO = 8;
     private String selectedItem = null;
 
     /**
@@ -67,15 +70,14 @@ public class BookFrame extends JInternalFrame {
 
         final RecipeLoader rl = new RecipeLoader();
 
-        this.setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        this.setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
         // SIZE BY DIMENSION
         this.setSize(FRAME_WIDTH, HEIGHTOFCELL * rl.getRecipeBook().getRecipeBookSize());
         //this.setSize(WIDTH, HEIGHT);
 
         //PATTERN PREVIEW GRID
         //The final GridPanel constructor will have as 3rd argument int SIZE_OF_CELL
-        final GridPanel pg = new GridPanel(FRAME_WIDTH / 3, FRAME_WIDTH / 3, null);
-
+        final GridPanel pg = new GridPanel(INITIAL_GRID_SIZE, INITIAL_GRID_SIZE, INITIAL_GRID_SIZE / GRID_TO_CELL_RATIO);
         // TEST FOR THE JList WITH A TEMP ARRAY
         final List<String> arrList = new ArrayList<String>();
 
@@ -93,36 +95,34 @@ public class BookFrame extends JInternalFrame {
                 setSelectedItem(list.getSelectedValue());
                 System.out.println("DEBUG | Selected Item: " + list.getSelectedValue());
                 final Matrix<Status> mat = new RLEConvert(rl.getRecipeBook().getRecipeByName(getSelectedItem()).getContent()).convert();
-                pg.paintGrid(mat.map(s -> s.equals(Status.ALIVE) ? Color.BLACK : Color.WHITE));
+                final Matrix<Status> newmat = new ListMatrix<Status>(pg.getGridHeight(), pg.getGridWidth(), () -> Status.DEAD);
+                Matrices.mergeXY(newmat, 0, 0, mat);
+                pg.paintGrid(newmat.map(s -> s.equals(Status.ALIVE) ? Color.BLACK : Color.WHITE));
             }
-
             @Override
             public void mouseClicked(final MouseEvent arg0) {
                 // TODO Auto-generated method stub
             }
-
             @Override
             public void mouseEntered(final MouseEvent arg0) {
                 // TODO Auto-generated method stub
             }
-
             @Override
             public void mouseExited(final MouseEvent arg0) {
                 // TODO Auto-generated method stub
             }
-
-
             @Override
             public void mouseReleased(final MouseEvent arg0) {
                 // TODO Auto-generated method stub
             }
 
         });
-
+        this.add(pg);
         this.add(list);
 
         //BUTTON PANEL
         final JPanel ioPanel = new JPanel();
+        ioPanel.setLayout(new BoxLayout(ioPanel, BoxLayout.Y_AXIS));
         this.add(ioPanel);
         final JButton placeBtn = new JButton("Place");
 
