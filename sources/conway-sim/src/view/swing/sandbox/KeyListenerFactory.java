ef1ccd0a366d0f865185dd8c4430e2777e4c9849
Evangelisti Davide <davide.evangelisti2@studio.unibo.it>
2018-03-30 10:59:53 +0200

    update keyListenerFacotry with new methode

--- sources/conway-sim/src/view/swing/sandbox/KeyListenerFactory.java
+++ sources/conway-sim/src/view/swing/sandbox/KeyListenerFactory.java
@@ -5,7 +5,6 @@ import javax.swing.AbstractAction;
 import javax.swing.ActionMap;
 import javax.swing.InputMap;
 import javax.swing.JComponent;
-import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 
 /**
@@ -13,19 +12,29 @@ import javax.swing.KeyStroke;
  */
 public final class KeyListenerFactory {
 
+    private static JComponent container;
+
     private KeyListenerFactory() { }
 
     /**
      * 
-     * @param panel the panel that should contain the keyListener 
+     * @param component the component for the key listener
+     */
+    public static void setContainer(final JComponent component) {
+        container = component;
+    }
+
+    /**
+     * 
+     * @param component the external component that should contain the keyListener 
      * @param name the unique name of this listener
      * @param keyCode the int (KeyEvent.VK_*) that start the event
      * @param modifier a bitwise-ored combination of any modifiers
      * @param event the event
      */
-    public static void addKeyListener(final JPanel panel, final String name, final int keyCode, final int modifier, final Runnable event) {
-        final InputMap inputMap = panel.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-        final ActionMap actionMap = panel.getActionMap();
+    public static void addKeyListener(final JComponent component, final String name, final int keyCode, final int modifier, final Runnable event) {
+        final InputMap inputMap = component.getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        final ActionMap actionMap = component.getActionMap();
         inputMap.put(KeyStroke.getKeyStroke(keyCode, modifier), name);
         actionMap.put(name, new AbstractAction() {
 
@@ -43,13 +52,13 @@ public final class KeyListenerFactory {
 
     /**
      * 
-     * @param panel the JPanel
+     * @param component the external component
      * @param name the name of this listener
      * @param keyCode the int (KeyEvent.VK_*) that start the event
      * @param event the event
      */
-    public static void addKeyListener(final JPanel panel, final String name, final int keyCode, final Runnable event) {
-        KeyListenerFactory.addKeyListener(panel, name, keyCode, 0, event);
+    public static void addKeyListener(final JComponent component, final String name, final int keyCode, final Runnable event) {
+        KeyListenerFactory.addKeyListener(component, name, keyCode, 0, event);
     }
 
 }
