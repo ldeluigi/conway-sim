861f5b41bed99eb94aa0bc9450a1921c9928b9c2
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2018-03-30 21:01:35 +0200

    Formatted javadoc

--- sources/conway-sim/src/core/model/GenerationFactory.java
+++ sources/conway-sim/src/core/model/GenerationFactory.java
@@ -13,11 +13,15 @@ import core.utils.Matrix;
  */
 public final class GenerationFactory {
 
-    private GenerationFactory() { }
+    private GenerationFactory() {
+    }
 
     /**
-     * A method to clone a {@link Generation}. Note that cells are copied with {@link Cell#copy()}, whereas the {@link Environment} is the exact same.
-     * @param generation is the generation to clone
+     * A method to clone a {@link Generation}. Note that cells are copied with
+     * {@link Cell#copy()}, whereas the {@link Environment} is the exact same.
+     * 
+     * @param generation
+     *            is the generation to clone
      * @return the cloned generation
      */
     public static Generation copyOf(final Generation generation) {
@@ -25,16 +29,22 @@ public final class GenerationFactory {
     }
 
     /**
-     * Creates a new {@link Generation} from a given {@link Matrix<Cell>} and an {@link Environment} of the same dimensions, without making copies.
-     * @param cellMatrix the {@link Matrix<Cell>}
-     * @param e the {@link Environment}
+     * Creates a new {@link Generation} from a given {@link Matrix<Cell>} and an
+     * {@link Environment} of the same dimensions, without making copies.
+     * 
+     * @param cellMatrix
+     *            the {@link Matrix<Cell>}
+     * @param e
+     *            the {@link Environment}
      * @return the {@link Generation} created from the arguments
      */
     public static Generation from(final Matrix<Cell> cellMatrix, final Environment e) {
         Objects.requireNonNull(cellMatrix);
         Objects.requireNonNull(e);
         if (cellMatrix.getHeight() != e.getHeight() || cellMatrix.getWidth() != e.getWidth()) {
-            throw new IllegalArgumentException("Cell Matrix and Environment must have the same dimensions.");
+            throw new IllegalArgumentException(
+                    "Cell Matrix and Environment must have the same dimensions. (" + cellMatrix.getWidth() + ","
+                            + cellMatrix.getHeight() + " - " + e.getWidth() + "," + e.getHeight() + ")");
         }
         return new Generation() {
 
