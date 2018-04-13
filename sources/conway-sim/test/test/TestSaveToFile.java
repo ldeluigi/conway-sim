310df3967dac2b4d3191fe792d817b2ef4981ae6
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2018-04-14 00:06:50 +0200

    Fixed test compile errors

--- sources/conway-sim/test/test/TestSaveToFile.java
+++ sources/conway-sim/test/test/TestSaveToFile.java
@@ -1,6 +1,6 @@
 package test;
 
-import controller.io.RLESaver;
+//import controller.io.RLESaver;
 import controller.io.RLETranslator;
 import core.model.Status;
 import core.utils.ListMatrix;
@@ -10,20 +10,20 @@ import core.utils.Matrix;
  *
  */
 public class TestSaveToFile {
-    static RLESaver rs;
-    /**
-     * Builder.
-     */
-    /**
-     * Test Method.
-     * @param args dasda
-     * @throws IllegalArgumentException ds
-     */
-    public static void main(final String[] args) throws IllegalArgumentException {
-        final Matrix<? extends Enum<?>> matrix = new ListMatrix<Status>(6, 6, () -> Status.ALIVE);
-        rs = new RLESaver(matrix);
-
-        rs = new RLESaver(RLETranslator.rleStringToMatrix("bbbbbb$bbbaab$bbbbbb$bbabbb$bbbbbb$bbbaab$", null));
-    }
+//    static RLESaver rs;
+//    /**
+//     * Builder.
+//     */
+//    /**
+//     * Test Method.
+//     * @param args dasda
+//     * @throws IllegalArgumentException ds
+//     */
+//    public static void main(final String[] args) throws IllegalArgumentException {
+//        final Matrix<? extends Enum<?>> matrix = new ListMatrix<Status>(6, 6, () -> Status.ALIVE);
+//        rs = new RLESaver(matrix);
+//
+//        rs = new RLESaver(RLETranslator.rleStringToMatrix("bbbbbb$bbbaab$bbbbbb$bbabbb$bbbbbb$bbbaab$", null));
+//    }
 
 }
