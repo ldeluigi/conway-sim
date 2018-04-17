61a076a608d6984ca057f40c85053f00a05aa4e0
Evangelisti Davide <davide.evangelisti2@studio.unibo.it>
2018-04-17 11:21:11 +0200

    refactor source position

---
+++ sources/conway-sim/src/view/Sandbox.java
@@ -0,0 +1,50 @@
+package view.swing;
+import javax.swing.JButton;
+import controller.editor.PatternEditor;
+/**
+ * 
+ *
+ */
+public interface Sandbox {
+    /**
+     * Applies size changes to the grid.
+     */
+    void resetGrid();
+
+    /**
+     * Enables "clear" button to be clicked by the user.
+     * @param flag
+     *            a boolean flag
+     */
+    void setButtonClearEnabled(boolean flag);
+
+    /**
+     * Enables "apply" button to be clicked by the user.
+     * @param flag
+     *            a boolean flag
+     */
+    void setButtonApplyEnabled(boolean flag);
+
+    /**
+     * Enables clear button to be clicked by the user.
+     * @return the book button
+     */
+    JButton getButtonBook();
+
+    /**
+     * Gets the editor used to handle the grid and returns it.
+     * @return the gridEtitor
+     */
+    PatternEditor getGridEditor();
+
+    /**
+     * Refreshes generation panel.
+     */
+    void refreshView();
+
+    /**
+     * 
+     * @param flag enable the save button
+     */
+    void setButtonSaveEnable(boolean flag);
+}
