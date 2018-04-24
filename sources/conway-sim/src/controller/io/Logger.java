3e48290cbf09f6fef263e50c85fcc45316672aad
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2018-04-24 18:27:21 +0200

    Added the possibility to change the log output for reusability

--- sources/conway-sim/src/controller/io/Logger.java
+++ sources/conway-sim/src/controller/io/Logger.java
@@ -4,17 +4,24 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 /**
- * Logger that outputs string to a file if available or to {@link System#err}.
+ * Logger that outputs string to the selected LogOutput if available or to
+ * {@link System#err}. The default LogOutput is a file output stream that
+ * targets a file specified in properties (log.file.name)
  */
 public final class Logger {
+
+    private static Optional<Supplier<PrintStream>> customOut = Optional.empty();
 
     private Logger() {
     }
 
     /**
-     * Logs the message to a file if possible or else to {@link System#err}, with the current time.
+     * Logs the message to the LogOutput if possible or else to {@link System#err},
+     * with the current time.
      * 
      * @param message
      *            the message to log
@@ -24,7 +31,7 @@ public final class Logger {
     }
 
     /**
-     * Logs the message to a file if possible or else to {@link System#err}.
+     * Logs the message to the LogOutput if possible or else to {@link System#err}.
      * 
      * @param message
      *            the message to log
@@ -38,8 +45,8 @@ public final class Logger {
     }
 
     /**
-     * Prints (formatted) the {@link Throwable} stack trace to a file if possible or else to
-     * {@link System#err}.
+     * Prints (formatted) the {@link Throwable} stack trace to the LogOutput if
+     * possible or else to {@link System#err}.
      * 
      * @param e
      *            the throwable to log
@@ -54,13 +61,31 @@ public final class Logger {
         }
     }
 
+    /**
+     * For each log request, the printStreamSupplier must produce a non-null
+     * PrintStream that will be used as LogOutput. If this method is never called,
+     * if it's called after the first log request, or if null is produced by the
+     * supplier, the default LogOutput will be used instead.
+     * 
+     * @param printStreamSupplier
+     *            the supplier of {@link PrintStream} for LogOutput
+     */
+    public static synchronized void setLogOutput(final Supplier<PrintStream> printStreamSupplier) {
+        customOut = Optional.ofNullable(printStreamSupplier);
+    }
+
     private static PrintStream err() {
         System.err.println("Couldn't create a log file in " + System.getProperty("user.dir"));
         return System.err;
     }
 
-    private static PrintStream out() throws FileNotFoundException {
-        return new PrintStream(
-                new FileOutputStream(ResourceLoader.loadString("log.file.name"), true), true);
+    private static synchronized PrintStream out() throws FileNotFoundException {
+        if (customOut.isPresent()) {
+            final PrintStream ps = customOut.get().get();
+            if (ps != null) {
+                return ps;
+            }
+        }
+        return new PrintStream(new FileOutputStream(ResourceLoader.loadString("log.file.name"), true), true);
     }
 }
